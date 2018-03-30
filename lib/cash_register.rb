bdf69eabd81035d69d9b08888d701d6bc83220fa
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-30 17:24:41 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
     @price = price
     @quantity = quantity
     @total += price*quantity
-    self.item = @item.split*@quantity.flatten
+    self.item = @item.split*@quantity
     @items << self.item
   end
 
